88c13ef98453a5eeea408d463bcc248fa5d8db61
Shaik Afzal <30412327+afzyshaaik@users.noreply.github.com>
2020-10-01 17:37:36 +0530

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+import {GlobalProvider} from './src/context/globalContext'
+
+export const wrapRootElement = ({ element }) => {
+    return <GlobalProvider>{element}</GlobalProvider>
+  }
